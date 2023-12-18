a78df474609deddf56e86941de9c16af31c4c75b
Philip Clark <ContactPhilipClark@protonmail.com>
2023-12-18 15:56:42 -0500

    add jsdocs

--- src/components/welcomeTile/Welcome.js
+++ src/components/welcomeTile/Welcome.js
@@ -2,6 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './welcome.css';
 
+/**
+ * Renders a welcome tile component.
+ *
+ * @param {Object} props - The component props.
+ * @param {Function} props.openController - The function to open the controller.
+ * @returns {JSX.Element} The rendered welcome tile component.
+ */
 const Welcome = ({ openController }) => {
   const handleButtonClick = () => {
     openController(true);
